769e071dc357d6e7f5eb54ebf2518efe483bad6e
Alexander Belousov <sunyawhite12@gmail.com>
2020-02-22 19:02:10 +0300

    Added MainApplication to project. Edited Manifest. Added Koin to dependencies

---
+++ app/src/main/java/com/github/sunyawhite/notereader/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.github.sunyawhite.notereader
+
+import android.app.Application
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
