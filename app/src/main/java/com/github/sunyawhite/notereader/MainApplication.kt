9f88f846b4f3453b52fe435ffa59d439f56058f9
Alexander Belousov <sunyawhite12@gmail.com>
2020-02-22 19:49:49 +0300

    Added repositoryModule, added MainApplication class

--- app/src/main/java/com/github/sunyawhite/notereader/MainApplication.kt
+++ app/src/main/java/com/github/sunyawhite/notereader/MainApplication.kt
@@ -1,11 +1,14 @@
 package com.github.sunyawhite.notereader
 
 import android.app.Application
+import com.github.sunyawhite.notereader.Koin.repositoryModule
 import org.koin.core.context.startKoin
 
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin { repositoryModule }
     }
 }
